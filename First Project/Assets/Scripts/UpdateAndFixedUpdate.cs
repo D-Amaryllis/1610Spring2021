5880572ffbc27cd884b0dd8dd0c0194a7b0eea5d
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-02 16:35:59 -0700

    Completed AwakeAndStart and UpdateAndFixedUpdate

---
+++ First Project/Assets/Scripts/UpdateAndFixedUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    void FixedUpdate()
+    //Called every physcis step
+    //FixedUpdate intervals are consistent
+    //used for regular updates such as:
+    // Adjusting physics (Ridgidbody) objects
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
+    }
+
+    void Update()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
