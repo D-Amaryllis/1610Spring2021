5b747cfcef56b3fe053dd11e37903fd1d26af6bc
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-05 11:54:03 -0700

    begining scripting lessons

---
+++ First Project/Assets/Scripts/KeyInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour
+{
+    public GUITexture graphic;
+    public Texture2D standard;
+    public Texture2D downgfx;
+    public Texture2D upgfx;
+    public Texture2D heldgfx;
+
+    void Start()
+    {
+        graphic.texture = standard;
+    }
+
+    void Update()
+    {
+        bool down = Input.GetKeyDown(KeyCode.Space);
+        bool held = Input.GetKey(KeyCode.Space);
+        bool up = Input.GetKeyUp(KeyCode.Space);
+
+        if (down)
+        {
+            graphic.texture = downgfx;
+        }
+        else if (held)
+        {
+            graphic.texture = standard;
+        }
+        else
+        {
+            graphic.texture = standard;
+        }
+
+        GUIText.text = " " + down + "\n " + held + "\n " + up;
+    }
+}
