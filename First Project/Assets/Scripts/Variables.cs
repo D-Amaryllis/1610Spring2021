80df1b442eda570464dc9eda5038c2cbbb4482a9
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-11 22:54:01 -0700

    Worked on Variables

--- First Project/Assets/Scripts/Variables.cs
+++ First Project/Assets/Scripts/Variables.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Variables : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /* variables are storage for data in the memory of the script.
+     Like labelled storage tubs.
+     
+    int num = 100;           a whole number
+    float rate = 10.2f;      a number with a decimal???
+    char code = 'C';         just one character
+    bool isValid = true;     bool can only be true or false
+    string name = "Steve";   just some letters, my dude.*/
+
+    private int cents = 60;
+
+    void Start ();
     {
-        
+        Debug.Log("I need 69 cents, and I have:" + cents);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update();
     {
-        
+        if (Input.GetKey(KeyCode.Space)) ;
+        {
+            Debug.Log("I have:" + cents);
+
+            if (cents < 69) ;
+            {
+                cents++;
+                Debug.Log("I add a penny");
+            }
+            if (cents = 69)
+            {
+                Debug.Log("Nice");
+            }
+        }
     }
 }
