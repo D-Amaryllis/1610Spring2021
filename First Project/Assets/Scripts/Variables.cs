1f9c7b308919b3f3d6f60122ca20adbb18b8bed6
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-12 12:19:26 -0700

    Completed Variables

--- First Project/Assets/Scripts/Variables.cs
+++ First Project/Assets/Scripts/Variables.cs
@@ -8,33 +8,34 @@ public class Variables : MonoBehaviour
     /* variables are storage for data in the memory of the script.
      Like labelled storage tubs.
      
-    int num = 100;           a whole number
-    float rate = 10.2f;      a number with a decimal???
-    char code = 'C';         just one character
+    int num = 100;           Integers (whole numbers), without decimals.
+    float rate = 10.2f;      Stores floating numbers with decimals, such as 19.99.
+    double yeah = 10.2f;     Don't use this.  It is the same as float, but its more accurate and takes longer to calculate.  Float is faster.
+    char code = 'C';         just one character 'c' 'b'
     bool isValid = true;     bool can only be true or false
-    string name = "Steve";   just some letters, my dude.*/
+    string name = "Steve";   just some letters, my dude.  "Hello world!"*/
 
     private int cents = 60;
 
-    void Start ();
+    void Start ()
     {
-        Debug.Log("I need 69 cents, and I have:" + cents);
+        Debug.Log("I need 69 cents, and I have " + cents + "cents");
     }
 
-    void Update();
+    void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) ;
+        if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("I have:" + cents);
+            Debug.Log("I have " + cents + "cents");
 
-            if (cents < 69) ;
+            if (cents < 69)
             {
                 cents++;
                 Debug.Log("I add a penny");
             }
-            if (cents = 69)
+            if (cents == 69)
             {
-                Debug.Log("Nice");
+                Debug.Log("6Nice9");
             }
         }
     }
