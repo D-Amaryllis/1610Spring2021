42a227d9015759751398cf6456fc0f848202d9af
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-17 11:36:33 -0700

    Made BB8

--- First Project/Assets/Scripts/UserInput.cs
+++ First Project/Assets/Scripts/UserInput.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 public class UserInput : MonoBehaviour
 {
    public Rigidbody2D ballRigidbody2D;
+   public float speed = 5f;
+   public float jumpForce = 300f;
+   
    public Vector2 direction;
    public Vector2 yDirection;
-   public float speed = 3f;
 
    void Update()
    {
-      /*
-       *  direction.x = Input.GetAxis("Horizontal");  <<for Joystick or pressing two arrows at the same time.
-       */
-      if (Input.GetKey(KeyCode.RightArrow))
+      direction.x = Input.GetAxis("Horizontal") * speed;                //<<for Joystick or pressing two arrows at the same time.
+      ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+       
+      /*if (Input.GetKey(KeyCode.RightArrow))
       {
          ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
       }
@@ -24,9 +26,11 @@ public class UserInput : MonoBehaviour
       {
          ballRigidbody2D.AddForce(-direction, ForceMode2D.Force);
       }
+         expanded version of joystick code above.  they do the same thing*/
 
-      if (Input.GetButtonDown("Jump"))
+      if (Input.GetButtonDown("Jump")) //GetButtonDown only reads the press once.  GetKey means you gotta hold it down to keep reading it.
       {
+         yDirection.y = jumpForce;
          ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
       }
    }
