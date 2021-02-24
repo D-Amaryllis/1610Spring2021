f97079ed019567a76bda7b50db1d1a37d620b13f
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-24 10:51:53 -0700

    Created conditionals

---
+++ First Project/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    
+    public int a, b, c;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (a + b == c)
+        {
+           print(true); 
+        }
+        else
+        {
+            print(false);
+        }
+    }
+    
+}
