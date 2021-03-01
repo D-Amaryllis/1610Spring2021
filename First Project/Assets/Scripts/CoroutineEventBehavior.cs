4395e044c3695fae45a56cec44ecc22c60da1ebe
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-28 20:34:24 -0700

    finished countdown practice

---
+++ First Project/Assets/Scripts/CoroutineEventBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class coroutineEventBehavior : MonoBehaviour
+{
+    public int number, maxNumber;
+    public float holdTime = 1f;
+    private WaitForSeconds wfsObj;
+    public UnityEvent startEvent;
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(holdTime);
+
+        while (number < maxNumber)
+        {
+            startEvent.Invoke();
+            yield return wfsObj;
+        }
+    }
+}
--- First Project/Assets/Scripts/CountdownBehavior.cs
+++ First Project/Assets/Scripts/CountdownBehavior.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountdownBehavior : MonoBehaviour
 {
-    public int number;
-    
+    public int number = 3;
+    public float holdTime = 1f;
+    public Text countDownText;
+
+    private WaitForSeconds wfsObj;
+
     IEnumerator Start()
     {
-        while (number < 4)
+        wfsObj = new WaitForSeconds(holdTime);
+        
+        while (number > 0)
         {
-            yield return new WaitForSeconds(0.5f);
-            print("Hello");
-            number++;
+            countDownText.text = number.ToString();
+            number--;
+            yield return wfsObj;
         }
+
+        countDownText.text = "Go!";
+
+        yield return wfsObj;
+
+        countDownText.text = null;
     }
 }
