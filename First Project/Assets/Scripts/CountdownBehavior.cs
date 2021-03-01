b342aa9cba2ad4b03e29857b2e11535e49888a8f
Davey Amaryllis <10696911@my.uvu.edu>
2021-02-28 20:03:40 -0700

    While loop practice

---
+++ First Project/Assets/Scripts/CountdownBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownBehavior : MonoBehaviour
+{
+    public int number;
+    
+    IEnumerator Start()
+    {
+        while (number < 4)
+        {
+            yield return new WaitForSeconds(0.5f);
+            print("Hello");
+            number++;
+        }
+    }
+}
