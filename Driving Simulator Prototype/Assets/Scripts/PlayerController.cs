bb6d0c9794ab2c2617bc5499533dbf1548d1f4f8
Davey Amaryllis <10696911@my.uvu.edu>
2021-03-03 11:12:29 -0700

    completed High Speed Chase

--- Driving Simulator Prototype/Assets/Scripts/PlayerController.cs
+++ Driving Simulator Prototype/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
-    {
-    }
+    public float speed = 5.0f;
 
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
