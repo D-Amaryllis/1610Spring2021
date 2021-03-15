97aa525089ab7c8343d81615ea58b5f77f1a2ef4
Davey Amaryllis <10696911@my.uvu.edu>
2021-03-15 11:02:29 -0600

    Fixed challenge1 player controls

--- Driving Simulator Prototype/Assets/Challenge 1/Scripts/CPlayerControllerX.cs
+++ Driving Simulator Prototype/Assets/Challenge 1/Scripts/CPlayerControllerX.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerX : MonoBehaviour
+public class CPlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
@@ -21,9 +21,9 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
