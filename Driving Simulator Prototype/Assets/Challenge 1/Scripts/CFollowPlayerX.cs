aa8c7d3276c9b2c81e8cc427a88ea27a65e0a135
Davey Amaryllis <10696911@my.uvu.edu>
2021-03-15 11:19:18 -0600

    Completed Chalenge 1

--- Driving Simulator Prototype/Assets/Challenge 1/Scripts/CFollowPlayerX.cs
+++ Driving Simulator Prototype/Assets/Challenge 1/Scripts/CFollowPlayerX.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
+public class CFollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    public Vector3 offset = new Vector3 (0,0,0);
 
     // Start is called before the first frame update
     void Start()
