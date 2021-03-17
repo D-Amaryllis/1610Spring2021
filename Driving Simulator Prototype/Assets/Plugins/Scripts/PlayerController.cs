d20305577e4ceb45569d470befc07e2dd4a2fb34
Davey Amaryllis <10696911@my.uvu.edu>
2021-03-16 19:25:11 -0600

    Added Prototype 2 files

---
+++ Driving Simulator Prototype/Assets/Plugins/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   float speed = 20.0f;
+   float turnSpeed = 45.0f;
+   float horizontalInput;
+   float forwardInput;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
