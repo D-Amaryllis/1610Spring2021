dab86beb0bd0432d7bb5d38eacdbf1bb0f61667b
Davey Amaryllis <10696911@my.uvu.edu>
2021-03-19 19:28:42 -0600

    Completed 2.4

--- Driving Simulator Prototype/Assets/Animal charge prototype/Course Library/Scripts/SpawnManager.cs
+++ Driving Simulator Prototype/Assets/Animal charge prototype/Course Library/Scripts/SpawnManager.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public float SpawnInterval = 1.5f;
+    public float StartDelay = 2;
+    
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", StartDelay, SpawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex],spawnPos, 
-                animalPrefabs[animalIndex].transform.rotation);
-        }
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        // Randomly generate animal index and spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+            
+        Instantiate(animalPrefabs[animalIndex],spawnPos, 
+            animalPrefabs[animalIndex].transform.rotation);
     }
 }
