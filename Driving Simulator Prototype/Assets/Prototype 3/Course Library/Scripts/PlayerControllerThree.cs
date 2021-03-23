dd50bf54beea5ab7f9c41a0fa4b12d39047b7eab
Davey Amaryllis <10696911@my.uvu.edu>
2021-03-23 10:54:48 -0600

    Started 3.1

---
+++ Driving Simulator Prototype/Assets/Prototype 3/Course Library/Scripts/PlayerControllerThree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerThree : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+    }
+}
