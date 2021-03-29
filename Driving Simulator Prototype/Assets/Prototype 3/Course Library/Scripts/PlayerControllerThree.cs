439faac2e8425c0abee20241c94a0e88a7e699f5
Davey Amaryllis <10696911@my.uvu.edu>
2021-03-28 23:11:39 -0600

    completed 3.3

--- Driving Simulator Prototype/Assets/Prototype 3/Course Library/Scripts/PlayerControllerThree.cs
+++ Driving Simulator Prototype/Assets/Prototype 3/Course Library/Scripts/PlayerControllerThree.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControllerThree : MonoBehaviour
 {
     private Rigidbody playerRB;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -13,15 +14,17 @@ public class PlayerControllerThree : MonoBehaviour
     {
         playerRB = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -35,6 +38,8 @@ public class PlayerControllerThree : MonoBehaviour
             {
                 gameOver = true;
                 Debug.Log("Game Over!");
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
             }
         }
     }
